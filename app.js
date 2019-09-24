18859db0bd72e2d1ba7b98e31112d74eeae4e80a
snymanje <jean.snyman@gmail.com>
2019-09-24 06:48:17 +0200

    commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const graphqlHTTP = require("express-graphql");
+const schema = require("./schema/schema.js");
+const mongoose = require("mongoose");
+
+const app = express();
+
+//connect to mongodb atlas
+mongoose.connect(
+  "mongodb://test:test@cluster0-shard-00-00-wwzcb.mongodb.net:27017,cluster0-shard-00-01-wwzcb.mongodb.net:27017,cluster0-shard-00-02-wwzcb.mongodb.net:27017/gql-ninja?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+);
+
+mongoose.connection.once("open", () => {
+  console.log("we're connected!");
+});
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: true
+  })
+);
+
+app.listen(4000, () => {
+  console.log("Server listening on port 4000");
+});
